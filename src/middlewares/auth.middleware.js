c12da2eb0ad92445d59bd17cc87d61e60b69dbea
MM-27-dev <monikamuniraju27@gmail.com>
2025-07-03 08:13:39 +0530

    bug fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,6 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
  
    const token = req.cookies.accessToken || req.body.accessToken;
+   console.log("Access Token:", token);
 
   if (!token) {
     throw new ApiError(401, "Unauthorized");
