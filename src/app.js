d907966dc8823089fa17368e3b1e69cdcbad51fc
MM-27-dev <monikamuniraju27@gmail.com>
2025-07-03 06:41:27 +0530

    added route files to app.js

--- src/app.js
+++ src/app.js
@@ -33,6 +33,25 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+
+// ✅ Serve static files
 app.use(express.static("public"));
+
+// ✅ Import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
+import { errorHandler } from "./middlewares/error.middlerware.js";
+
+// ✅ Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running!");
+});
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+
+// ✅ Global error handler
+app.use(errorHandler);
 
 export { app };
