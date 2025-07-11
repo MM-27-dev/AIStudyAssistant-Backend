7573f642eae532bb7b36ca5951ad67f84c9fd51b
MM-27-dev <monikamuniraju27@gmail.com>
2025-07-11 15:49:07 +0530

    fix bug - Speclling mistake

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,13 +7,20 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
  
    const token = req.cookies.accessToken || req.body.accessToken;
    
+   if (!process.env.ACCESS_TOKEN_SECRET) {
+     console.error("❌ ACCESS_TOKEN_SECRET is missing!");
+   }
+   if (!process.env.ACCESS_TOKEN_EXPIRY) {
+     console.error("❌ ACCESS_TOKEN_EXPIRY is missing!");
+   }
+  
 
   if (!token) {
     throw new ApiError(401, "Unauthorized");
   }
 
   try {
-    const decodedToken = jwt.verify(token, process.env.ACCES_TOKEN_SECRET);
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     console.log("Verify token", decodedToken);
     
