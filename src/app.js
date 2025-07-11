1cf421d6bf4c6ea8987e2d554a90df5e3d9ffb38
MM-27-dev <monikamuniraju27@gmail.com>
2025-07-11 07:49:17 +0530

    fix the bug

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(helmet());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-// ✅ Serve static files
+// ✅ Serve static files in app
 app.use(express.static("public"));
 
 
